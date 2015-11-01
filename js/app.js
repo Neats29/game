69910e30007055c24a211a4b5443606e87b6b253
Anita Amini <baadge29@yahoo.co.uk>
2015-11-01 13:23:06 +0000

    small additions

--- js/app.js
+++ js/app.js
@@ -4,7 +4,20 @@
 var person = [];
 var computer = [];
 
+var personScore = [];
+var computerScore = [];
+
 var winningRows = [ [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7] ];
+
+
+////counting score
+//function countScore() {
+//	
+//}
+//
+
+
+
 
 
 //render image when clicked
